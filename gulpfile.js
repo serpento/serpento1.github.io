3569c5a3755a65ec8ea7781d796b04b41361b5d8
serpento <simsonne@tut.by>
2016-02-10 23:37:37 +0300

    HW-3 Update template cache to fit path

--- gulpfile.js
+++ gulpfile.js
@@ -8,45 +8,56 @@ var templateCache = require('gulp-angular-templatecache');
 
 gulp.task('clean', function () {
   return gulp.src( [
-       './tmp'
-      ,'./dist'
+       'tmp'
+      ,'dist'
     ]
     , {read: false})
     .pipe(clean());
 });
 
 gulp.task('templateCache', ['clean'], function () {
-  return gulp.src('./app/partials/**/*.html')
-    .pipe(templateCache())
-    .pipe(gulp.dest('./tmp'));
+  return gulp.src('app/partials/*.html')
+    .pipe(templateCache({
+      module: 'sumModule',
+      root: 'partials'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 
-gulp.task('concat', ['templateCache'], function() {
+gulp.task('compile-app', ['templateCache'], function() {
   return gulp.src([
-    './bower_components/angular/angular.js'
-    , './bower_components/underscore/underscore.js'
-    , './bower_components/angular-route/angular-route.js'
-    , './tmp/**/*.js'
-    , './app/**/*.js'
+    'app/**/*.js'
   ])
     .pipe(ngAnnotate())
     .pipe(concat('main.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('inject', ['concat'], function () {
-  var target = gulp.src('./app/index.html');
+
+gulp.task('compile-vendor', function() {
+  return gulp.src([
+    'bower_components/angular/angular.js'
+    , 'bower_components/underscore/underscore.js'
+    , 'bower_components/angular-route/angular-route.js'
+  ])
+    .pipe(concat('angular-vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('inject', ['compile-app', 'compile-vendor'], function () {
+  var target = gulp.src('app/index.html');
 
   var sources = gulp.src([
-    , './dist/**/*.js'
-    , './bower_components/bootstrap/dist/css/bootstrap.css'
-    , './app/**/*.css'
+    , 'dist/**/*.js'
+    , 'bower_components/bootstrap/dist/css/bootstrap.css'
+    , 'app/**/*.css'
   ], { read: false });
 
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./dist'));
+  return target.pipe(inject(sources, { read: false, ignorePath: '/dist/', addRootSlash: false }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['inject'], function() {
