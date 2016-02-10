c679fcd14a1e67a6b102e85aa86ae095c7f46436
serpento <simsonne@tut.by>
2016-02-10 22:28:37 +0300

    HW-3 Add gulp tasks to preprocess source files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,48 @@
 var gulp = require('gulp');
+var ngAnnotate = require('gulp-ng-annotate');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var inject = require('gulp-inject');
+var clean = require('gulp-clean');
 
-gulp.task('inject', function () {
-  var target = gulp.src('./app/index.html');
-  // It's not necessary to read the files (will speed up things), we're only after their paths:
-  var sources = gulp.src([
+gulp.task('clean', function () {
+  return gulp.src( [
+       './temp'
+      ,'./dist'
+    ]
+    , {read: false})
+    .pipe(clean());
+});
+
+gulp.task('concat', function() {
+  return gulp.src([
+    './app/**/*.js'
     , './bower_components/angular/angular.js'
     , './bower_components/underscore/underscore.js'
     , './bower_components/angular-route/angular-route.js'
-    , './app/**/*.js'
+  ])
+    .pipe(ngAnnotate())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('inject', ['concat'], function () {
+  var target = gulp.src('./app/index.html');
+
+  var sources = gulp.src([
+    , './dist/**/*.js'
     , './bower_components/bootstrap/dist/css/bootstrap.css'
     , './app/**/*.css'
   ], { read: false });
 
   return target.pipe(inject(sources))
-    .pipe(gulp.dest('./app'));
+    .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('build', ['inject'], function() {
+
+});
+
+
+
