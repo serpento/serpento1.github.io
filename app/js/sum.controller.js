9f77aa867a455516e0499f47c6a1692cdc9ae6e8
serpento <simsonne@tut.by>
2016-02-12 00:25:00 +0300

    HW-3 Add exchange to CAD

--- app/js/sum.controller.js
+++ app/js/sum.controller.js
@@ -1,62 +1,64 @@
 angular.module("sumModule", ['ngRoute'])
 
-    .controller('sumCtrl', function($scope, $routeParams, $location, $http) {
-        $scope.list = {
-            summand1: parseInt($routeParams.summand1) || '',
-            summand2: parseInt($routeParams.summand2) || ''
+  .controller('sumCtrl', function($scope, $routeParams, $location, $http) {
+    $scope.list = {
+      summand1: parseInt($routeParams.summand1) || '',
+      summand2: parseInt($routeParams.summand2) || ''
+    };
+
+    $scope.setUrl = function() {
+      if (!$scope.list.summand1 || !$scope.list.summand2) return;
+      $location.path('/' + $scope.list.summand1 + '/' + $scope.list.summand2);
+    };
+
+    $http.get('http://api.fixer.io/latest')
+      .then(function successCallback(response) {
+        $scope.currencyRates = response.data.rates;
+      }, function errorCallback(response) {
+        console.log('Oops!');
+      });
+  })
+
+  .directive('laSum', function() {
+    return {
+      restrict: 'E',
+      scope:    { list: '=', currencyRates: '=' },
+      template: '<input type="text" readonly="true" value="{{ summarize() }}" />' +
+        '<span>{{ getSumInCurrency("CAD") }}</span> CAD',
+      link:     function(scope){
+
+        function count() {
+          return _.reduce(scope.list, function(memo, num){ return memo + num; }, 0);
+        }
+
+        function formMessage(count){ return "sum is: " + count; }
+        function logMessage(phrase){ console.log(phrase); }
+
+        var logSum = _.compose(logMessage, formMessage);
+
+        scope.summarize = function() {
+          logSum(count());
+          return count();
         };
 
-        $scope.setUrl = function() {
-            if (!$scope.list.summand1 || !$scope.list.summand2) return;
-            $location.path('/' + $scope.list.summand1 + '/' + $scope.list.summand2);
+        scope.getSumInCurrency = function(currency) {
+          return count()/scope.currencyRates[currency];
         };
+      }
+    };
+  })
 
-
-        $http.get('http://api.fixer.io/latest')
-          .then(function successCallback(response) {
-            console.log(response);
-          }, function errorCallback(response) {
-            console.log('Oops!');
-          });
-
-    })
-
-    .directive('laSum', function() {
-        return {
-            restrict: 'E',
-            scope: { list: '=' },
-            template: '<input type="text" readonly="true" value="{{ summarize() }}" />',
-            link: function(scope){
-                function count() {
-                    return _.reduce(scope.list, function(memo, num){ return memo + num; }, 0);
-                }
-
-                function formMessage(count){ return "sum is: " + count; }
-                function logMessage(phrase){ console.log(phrase); }
-
-                var logSum = _.compose(logMessage, formMessage);
-
-                scope.summarize = function() {
-                   logSum(count());
-                   return count();
-                }
-            }
-        };
-
-    })
-
-    .config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'partials/sum.html',
-                    controller: 'sumCtrl'
-                })
-                .when('/:summand1/:summand2', {
-                    templateUrl: 'partials/sum.html',
-                    controller: 'sumCtrl'
-                })
-                .otherwise({
-                    redirectTo: '/'
-                });
-        }])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/sum.html',
+        controller: 'sumCtrl'
+      })
+      .when('/:summand1/:summand2', {
+        templateUrl: 'partials/sum.html',
+        controller: 'sumCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
